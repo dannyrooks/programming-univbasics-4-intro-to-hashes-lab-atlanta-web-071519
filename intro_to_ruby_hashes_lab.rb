2f2c9a275dc56e6de4672f3651eda830022068f1
Danny Rooker <dannyrooks@gmail.com>
2019-07-11 18:27:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 
 def my_hash_creator(key, value)
   my_hash_creator = {
-    :name = 1, "Grace Hopper" = 2
+    key
   }
   # return a hash that includes the key and value parameters passed into this method
 end
